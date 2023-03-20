811b35e86e0c5b49965f0b9ff3bcbf46661f2778
Erfan Goudarzi <erfangoudarzi@gmail.com>
2023-03-20 09:29:19 -0700

    New method of FUnction

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,10 @@
 // function definition 
-function greet (name) {
-    console.log(`Hello ${name}`)
-}
-greet(Erfan) 
+// function greet (name) {
+//     console.log(`Hello ${name}`)
+// }
+
+// Function Calling 
+// greet("Erfan") 
+const name = (fname) => console.log(`Hello ${fname}`)
+name("Erfan")
+// Name is Erfan And I am so Happy to meet you all  
