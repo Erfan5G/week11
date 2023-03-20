09060c6fb0c1f93e06f12e9e53f83a778d1227d5
Erfan Goudarzi <erfangoudarzi@gmail.com>
2023-03-20 10:10:18 -0700

    Roundin the number in js with -->Tofixed()

--- scripts/script.js
+++ scripts/script.js
@@ -8,8 +8,25 @@
 // const name = (fname) => console.log(`Hello ${fname}`)
 // name("Erfan") 
 
-function hello (name ,time = "day") { 
-    console.log( `Hello ${name} good ${time} `) 
-}
-hello ('Erfan', "Boy")
+// function hello (name ,time = "day") { 
+//     console.log( `Hello ${name} good ${time} `) 
+// }
+// hello ('Erfan', "Boy")
 
+const enteredValue = prompt ("Enter the Value ") 
+function CalculateArea (radius) {
+    if (isNaN (radius) ) {
+        alert ("It is not a number")
+    }
+    else {
+        const area = Math.PI * radius * radius ; 
+        return area.toFixed(2) 
+    }
+}
+let result = CalculateArea(enteredValue) 
+if (result) {
+    console.log(result)
+}
+else {
+    console.log("Not good ")
+}
